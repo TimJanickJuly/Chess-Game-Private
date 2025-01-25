fcdab519e2e8b8231e9faca8a1948471509e3b45
TimJanickJuly <168122507+TimJanickJuly@users.noreply.github.com>
2025-01-25 20:11:40 +0100

    noice

--- chess-frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
-const App = () => <h1>Hello, React!</h1>;
-
-ReactDOM.render(<App />, document.getElementById('root'));
